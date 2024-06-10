e1ea6dba253e7257664e115a4950086de761dc54
FengXiang2233 <fjx13125@163.com>
2024-06-10 19:32:45 +0800

    Initial commit

---
+++ src/main/java/org/cneko/ctlib/spigot_language_kotlin/Spigot_Language_Kotlin.java
@@ -0,0 +1,17 @@
+package org.cneko.ctlib.spigot_language_kotlin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Spigot_Language_Kotlin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/org/cneko/ctlib/spigot_language_kotlin/ctlib/plugin/LibrariesLoader.java
@@ -0,0 +1,2 @@
+package org.cneko.ctlib.spigot_language_kotlin.ctlib.plugin;public class LibrariesLoader {
+}
