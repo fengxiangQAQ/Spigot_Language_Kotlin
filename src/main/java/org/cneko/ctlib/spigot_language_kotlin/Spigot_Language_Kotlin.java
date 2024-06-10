d6c5d61225a63aebf82f634513400f7bd017ec91
FengXiang2233 <fjx13125@163.com>
2024-06-10 19:48:57 +0800

    .

--- src/main/java/org/cneko/ctlib/spigot_language_kotlin/Spigot_Language_Kotlin.java
+++ src/main/java/org/cneko/ctlib/spigot_language_kotlin/Spigot_Language_Kotlin.java
@@ -1,17 +1,21 @@
 package org.cneko.ctlib.spigot_language_kotlin;
 
+import net.byteflux.libby.BukkitLibraryManager;
+import net.byteflux.libby.LibraryManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.cneko.ctlib.spigot_language_kotlin.ctlib.plugin.LibrariesLoader;
 
 public final class Spigot_Language_Kotlin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        // 初始化load
+        LibraryManager libraryManager;
+        libraryManager = new BukkitLibraryManager(this);
+        LibrariesLoader.setManager(libraryManager);
+        // 加载依赖项
+        LibrariesLoader.load("org.xerial", "sqlite-jdbc", "3.41.2.2");
+        LibrariesLoader.load("org.jetbrains.kotlin", "kotlin-stdlib", "2.0.0-Beta1");
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
