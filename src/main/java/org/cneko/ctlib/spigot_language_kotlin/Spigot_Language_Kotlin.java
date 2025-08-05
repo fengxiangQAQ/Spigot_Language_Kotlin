6c0689cc1f42e90a78ba94e8ed64735cfa8c2e0e
枫相 <140891522+FengXiang2233@users.noreply.github.com>
2025-08-05 20:59:25 +0800

    去除无用加载项

--- src/main/java/org/cneko/ctlib/spigot_language_kotlin/Spigot_Language_Kotlin.java
+++ src/main/java/org/cneko/ctlib/spigot_language_kotlin/Spigot_Language_Kotlin.java
@@ -14,7 +14,6 @@ public final class Spigot_Language_Kotlin extends JavaPlugin {
         libraryManager = new BukkitLibraryManager(this);
         LibrariesLoader.setManager(libraryManager);
         // 加载依赖项
-        LibrariesLoader.load("org.xerial", "sqlite-jdbc", "3.41.2.2");
         LibrariesLoader.load("org.jetbrains.kotlin", "kotlin-stdlib", "2.0.0-Beta1");
     }
 
